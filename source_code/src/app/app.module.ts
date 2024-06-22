87bf8980f4c201c7186b467acac15f5921c2668c
gemmeeting <gemmeeting@gmail.com>
2024-06-22 21:12:35 +0100

    new deploy - added programs

--- source_code/src/app/app.module.ts
+++ source_code/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { NgParticlesModule } from "ng-particles";
+import { NgParticlesModule } from 'ng-particles';
 import { TabEquipaComponent } from './pages/tab-equipa/tab-equipa.component';
 import { TabProgramaComponent } from './pages/tab-programa/tab-programa.component';
 import { TabInscricoesComponent } from './pages/tab-inscricoes/tab-inscricoes.component';
@@ -18,30 +18,12 @@ import { TabFaqsComponent } from './pages/tab-faqs/tab-faqs.component';
 import { TabPatrociniosComponent } from './pages/tab-patrocinios/tab-patrocinios.component';
 import { TabContactosComponent } from './pages/tab-contactos/tab-contactos.component';
 import { ContentNovidadesEmBreveComponent } from './pages/content-novidades-em-breve/content-novidades-em-breve.component';
-
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomepageComponent,
-    TabEquipaComponent,
-    TabProgramaComponent,
-    TabInscricoesComponent,
-    TabResumosComponent,
-    TabFaqsComponent,
-    TabPatrociniosComponent,
-    TabContactosComponent,
-    ContentNovidadesEmBreveComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FontAwesomeModule,
-    SharedMainModule,
-    NgParticlesModule
-  ],
+  declarations: [AppComponent, HomepageComponent, TabEquipaComponent, TabProgramaComponent, TabInscricoesComponent, TabResumosComponent, TabFaqsComponent, TabPatrociniosComponent, TabContactosComponent, ContentNovidadesEmBreveComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, FontAwesomeModule, SharedMainModule, NgParticlesModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- source_code/src/app/pages/tab-programa/tab-programa.component.ts
+++ source_code/src/app/pages/tab-programa/tab-programa.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-tab-programa',
   templateUrl: './tab-programa.component.html',
-  styleUrls: ['./tab-programa.component.scss']
+  styleUrls: ['./tab-programa.component.scss'],
 })
-export class TabProgramaComponent implements OnInit {
+export class TabProgramaComponent {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  downloadPDF() {
+    this.http.get('./assets/pdfs/Programa_GEMMEETING_2024.pdf', { responseType: 'blob' }).subscribe((res: any) => {
+      const url = window.URL.createObjectURL(res);
+      saveAs(url, 'Programa_GEMMEETING_2024.pdf');
+    });
   }
-
 }
